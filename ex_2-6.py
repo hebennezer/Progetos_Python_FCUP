cc75bc60681259bec0cad14016636179063b44b5
hebennezer <hebennezer@gmail.com>
2023-05-25 17:10:10 +0100

    ex_2-6

--- ex_2-6.py
+++ ex_2-6.py
@@ -1,11 +1,16 @@
 import math
 
 xs = [5, 0, 42, 10, 24, 30, 81]
-
+# (a)
 for i in xs:
     print(i)
 
+#(b)
 for i in xs:
 	print(i, int(math.pow(i, 2)), int(math.sqrt(i)))
 
+#(c)
+total= 0
 for i in xs:
+    total += i
+    print(i, total)
