bab87e40fa0e200ee2db3c7a0b56563925d2c2cf
hebennezer <hebennezer@gmail.com>
2023-05-26 16:28:39 +0100

    square

--- square.py
+++ square.py
@@ -2,7 +2,7 @@ from turtle import *
 
 
 def square(side):
-    for c in ["red", "blue", "green", "black"]:
+    for c in ["red", "blue", "green", "white"]:
         color(c)
         forward(side)
         left(90)
@@ -10,6 +10,7 @@ def square(side):
 
 reset()
 speed(15)
+bgcolor("black")
 
 for i in range(50):
     square(50 + i * 5)
