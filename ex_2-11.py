7dcb2f19db90e3d83750344d6310304c9844ffc5
hebennezer <hebennezer@gmail.com>
2023-05-27 13:00:43 +0100

    fim aula 27 maio

---
+++ ex_2-11.py
@@ -0,0 +1,21 @@
+import turtle
+
+def friso(n, lado):
+    
+	turtle.speed(1)
+	turtle.color("white")
+	turtle.bgcolor("black")
+
+	for i in range(n):
+		turtle.forward(lado)
+		turtle.left(90)
+		turtle.forward(lado)
+		turtle.right(90)
+		turtle.forward(lado)
+		turtle.right(90)
+		turtle.forward(lado)
+		turtle.left(90)
+
+friso(3, 50)
+turtle.exitonclick()
+turtle.done()
