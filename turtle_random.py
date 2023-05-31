21ec7436a685ad8fafa98cf73f59428c14c67df4
hebennezer <hebennezer@gmail.com>
2023-05-31 16:58:29 +0100

    turtle_random

--- turtle_random.py
+++ turtle_random.py
@@ -1,19 +1,31 @@
 from random import *
 from turtle import *
 
+def visivel():
+    # Verifica se a turtle esta dentro da janela.
+    w = 0.5 * window_width()
+    h = 0.5 * window_height()
+    x = xcor()
+    y = ycor()
+    return ((x <= w) and (x >= -w) and (y <= h) and (y >= -h))
+
 def passeio(n, a):
     shape("turtle")
     bgcolor("black")
     color("white")
-    speed(1)
+    speed()
 
     step = 10
 
     for i in range(n):
         forward(step)
-        angle =  randint(-a, a)
-        left(angle)
+        if visivel():
+            angle =  randint(-a, a)
+            left(angle)
+        else:
+            angle = towards(0, 0)
+            setheading(angle)
 
-passeio(100, 30)
+passeio(300, 30)
 exitonclick()
 done()
